c4f62d007582778248b530a4fbc493875035adfe
blue <erlendlbr@gmail.com>
2023-12-09 21:37:06 +0100

    recursive

--- src/day_9/pyramid.rs
+++ src/day_9/pyramid.rs
@@ -13,35 +13,33 @@ pub fn get_top_num() {
         })
         .collect();
 
-    let num: i32 = data.iter().map(|vec| calc_vec_num(vec.as_slice())).sum();
-    println!("Top num is {}", num);
-    let rev: i32 = data
-        .iter()
-        .map(|vec| calc_vec_num_rev(vec.as_slice()))
-        .sum();
+    let rec_sum: i32 = data.iter().map(call_rec).sum();
+    println!("recursive calc: {}", rec_sum);
+    let rev: i32 = data.iter().map(calc_rev).sum();
     println!("the reverse is {}", rev);
 }
 
-fn calc_vec_num(vec: &[i32]) -> i32 {
-    let mut col_vec = vec![vec.to_owned()];
-    loop {
-        let look_up = col_vec.last().unwrap();
-        let work_vec: Vec<i32> = look_up
-            .iter()
-            .enumerate()
-            .filter(|tup| 0 < tup.0)
-            .map(|tup| tup.1 - look_up[tup.0 - 1])
-            .collect();
-        if work_vec.iter().all(|i| i == &0) {
-            break;
-        }
-        col_vec.push(work_vec);
-    }
-    col_vec.iter().map(|vec| vec.last().unwrap()).sum()
+fn calc_rev(vec: &Vec<i32>) -> i32 {
+    let mut rev = vec.clone();
+    rev.reverse();
+    call_rec(&rev)
 }
 
-fn calc_vec_num_rev(vec: &[i32]) -> i32 {
-    let mut rev = vec.to_owned();
-    rev.reverse();
-    calc_vec_num(rev.as_slice())
+fn call_rec(vec: &Vec<i32>) -> i32 {
+    vec.last().unwrap() + rec_calc(vec)
+}
+
+fn rec_calc(vec: &Vec<i32>) -> i32 {
+    let into: Vec<i32> = vec
+        .iter()
+        .enumerate()
+        .filter(|tup| 0 < tup.0)
+        .map(|tup| tup.1 - vec[tup.0 - 1])
+        .collect();
+
+    if into.iter().all(|i| i == &0) {
+        0
+    } else {
+        into.last().unwrap() + rec_calc(&into)
+    }
 }
